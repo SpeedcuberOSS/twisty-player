763e0ee5c1111dacc89c93a65cd1f8ec34c44765
Joseph Hale <me@jhale.dev>
2025-02-17 16:46:09 -0700

    feat: Add global `app`
    Now webviews can inject javascript to interact with
    `window.app` to create/control twisty puzzles.

--- src/main.ts
+++ src/main.ts
@@ -1,27 +1,64 @@
 // Always keep the following line if you are using any twisty players on your page.
 import "cubing/twisty";
-// Use the following line for specific imports from `cubing/twisty`.
-import { TwistyAlgViewer, type TwistyPlayer } from "cubing/twisty";
+import { Move } from "cubing/alg";
+import { TwistyPlayer, type PuzzleID } from "cubing/twisty";
 
-// Import from other modules as usual.
-import { randomScrambleForEvent } from "cubing/scramble";
+type Algorithm = string;
+type CSSColor = string;
+
+interface TwistyPlayerProps {
+  algorithm?: Algorithm;
+  puzzle?: PuzzleID;
+  visualization?: "3D" | "2D";
+  hintFacelets?: "floating";
+  backView?: "top-right" | "side-by-side";
+  backgroundColor?: CSSColor;
+}
 
 class App {
-  // Example of getting an element from the page.
-  twistyPlayer: TwistyPlayer = document.querySelector("#main-player")!;
-  // Example of creating a new element and adding it to the page.
-  twistyAlgViewer = document.body.appendChild(
-    new TwistyAlgViewer({ twistyPlayer: this.twistyPlayer }),
-  );
+  player: TwistyPlayer = document.querySelector("#main-player")!;
+  
   constructor() {
-    this.updateScramble();
+    this.player = this._createTwistyPlayer({});
   }
 
-  async updateScramble() {
-    this.twistyPlayer.alg = await randomScrambleForEvent("333");
+  _createTwistyPlayer(props: TwistyPlayerProps) {
+    const player = new TwistyPlayer({
+      puzzle: props.puzzle ?? "3x3x3",
+      visualization: props.visualization || "3D",
+      hintFacelets: props.hintFacelets || "none",
+      backView: props.backView || "none",
+      background: "none",
+      controlPanel: "none",
+    });
+    player.id = "twisty-player";
+    player.style.cssText = `width: 100vw; height: 100vh;`;
+    player.alg = props.algorithm || "";
+    return player;
+  }
+  
+  setTwistyPlayer(props: TwistyPlayerProps = {}) {
+    this.player = this._createTwistyPlayer(props);
+    document.getElementById("twisty-player").replaceWith(this.player);
+    document.body.style.backgroundColor = props.backgroundColor ?? "white";
+  }
+  
+  addMove(move: string) {
+    this.player.experimentalAddAlgLeaf(new Move(move));
+  }
+  
+  setAlgorithm(alg: string) {
+    this.player.alg = alg;
+  }
+  
+  version() {
+    return {
+      cubing: "0.46.2",
+      "twisty-player": "0.1.0",
+    };
   }
 }
 
 // Make the app object available in the console for debugging.
-// Try running: app.updateScramble()
+// Try running: app.setTwistyPlayer()
 globalThis.app = new App();
